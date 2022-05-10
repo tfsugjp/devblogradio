93fd03c4de7caec0cf11bd0fb26af681861c1cf2
kkamegawa <kkamegawa@clueup.org>
2022-05-10 18:40:08 +0900

    add script 1st version

--- feedsync.ps1
+++ feedsync.ps1
@@ -1,12 +1,16 @@
 $url = 'https://devblogs.microsoft.com/landingpage/'
 
 $number = (gh issue list -s open --json number | convertfrom-json).number
+$comments = (gh issue view $number -c --json comments | convertfrom-json).comments
+$lastupdate = [datetime]($comments | Sort-Object $_.createdAt -Bottom 1 | Select-Object createdAt).createdAt
 
 $feed =[xml](invoke-webrequest -Uri $url -UseBasicParsing)
-foreach ($item in $feed.rss.channel.item) {
+$notraymond = $feed.rss.channel.item | Where-Object {$_.creator -ne 'Raymond Chen' -and [datetime]$_.pubdate -gt $lastupdate}
+
+foreach ($item in $notraymond) {
     $title = $item.title
     $link = $item.link 
     $comment = "[$title]($link)"
 
-    gh issue comment -t $number -m $comment
+    gh issue comment $number -b $comment
 }
