2d5b87b97b70e58811e206af2580e211cc646aa5
kkamegawa <kkamegawa@clueup.org>
2022-05-12 18:54:02 +0900

    fix local timezone issue

--- feedsync.ps1
+++ feedsync.ps1
@@ -4,13 +4,17 @@ $number = (gh issue list -s open --json number | convertfrom-json).number
 $comments = (gh issue view $number -c --json comments | convertfrom-json).comments
 $lastupdate = [datetime]($comments | Sort-Object $_.createdAt -Bottom 1 | Select-Object createdAt).createdAt
 
+#gh issue returns local timezone
+$currenttimediff = (Get-TimeZone).baseutcoffset.hours
+$lastupdate = $lastupdate.addhours(-$currenttimediff.hours)
+
 $feed =[xml](invoke-webrequest -Uri $url -UseBasicParsing)
-$notraymond = $feed.rss.channel.item | Where-Object {$_.creator -ne 'Raymond Chen' -and [datetime]$_.pubdate -gt $lastupdate}
-
-foreach ($item in $notraymond) {
-    $title = $item.title
-    $link = $item.link 
-    $comment = "[$title]($link)"
-
-    gh issue comment $number -b $comment
+foreach ($item in $feed.rss.channel.item) {
+    $pubDate = [datetime]$item.pubDate
+    if($item.creator -ne 'Raymond Chen' -and $pubDate -gt $lastupdate) {
+        $title = $item.title
+        $link = $item.link 
+        $comment = "[$title]($link)"
+        gh issue comment $number -b $comment
+    }
 }
