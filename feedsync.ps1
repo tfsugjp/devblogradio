83f1878be2811879266ae6769093b731f715e0f8
KAMEGAWA Kazushi <kkamegawa@clueup.org>
2022-05-14 09:05:32 +0900

    fix: in case of comment is empty

--- feedsync.ps1
+++ feedsync.ps1
@@ -1,12 +1,17 @@
 $url = 'https://devblogs.microsoft.com/landingpage/'
-
 $number = (gh issue list -s open --json number | convertfrom-json).number
-$comments = (gh issue view $number -c --json comments | convertfrom-json).comments
-$lastupdate = [datetime]($comments | Sort-Object $_.createdAt -Bottom 1 | Select-Object createdAt).createdAt
+# updatedAt returns UTC, issue comments returns local time, why?
+$updatedAt = [datetime](gh issue view $number --json updatedAt | convertfrom-json).updatedAt
 
-#gh issue returns local timezone
-$currenttimediff = (Get-TimeZone).baseutcoffset.hours
-$lastupdate = $lastupdate.addhours(-$currenttimediff.hours)
+$comments = (gh issue view $number -c --json comments | convertfrom-json).comments
+if($comments.Count -gt 1){
+    $lastupdate = [datetime]($comments | Sort-Object $_.createdAt -Bottom 1 | Select-Object createdAt).createdAt
+    #gh issue returns local timezone
+    $currenttimediff = (Get-TimeZone).baseutcoffset.hours
+    $lastupdate = $lastupdate.addhours(-$currenttimediff.hours)
+}else{
+    $lastupdate = $updatedAt
+}
 
 $feed =[xml](invoke-webrequest -Uri $url -UseBasicParsing)
 foreach ($item in $feed.rss.channel.item) {
