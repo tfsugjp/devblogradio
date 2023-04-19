0c4752909fb3d36a092166465a83ab25057dbebf
KAMEGAWA Kazushi <kkamegawa@clueup.org>
2023-04-20 01:59:12 +0900

    Update test_wf.ps1

--- test_wf.ps1
+++ test_wf.ps1
@@ -1,10 +1,13 @@
-$apitoken = $ENV:OPENAI_API_TOKEN
-if([string]::isnullorempty($ENV:OPENAI_API_TOKEN) -eq $true){
+param(
+    [String] [Parameter (Mandatory=$true)] $apitoken,
+    [String] [Parameter (Mandatory=$true)] $apiUrl,
+)
+
+if([string]::isnullorempty($apitoken) -eq $true){
   write-host 'API Token is null'
 }
 
-$apiendpoint = $ENV:OPENAI_API_URL
-if([string]::isnullorempty($ENV:OPENAI_API_URL) -eq $true){
+if([string]::isnullorempty($apiUrl) -eq $true){
   write-host 'API endpoint is null'
 }else{
   write-host $apiendpoint
