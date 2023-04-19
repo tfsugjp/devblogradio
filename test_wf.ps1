5a8996985c25cafb62eb9115dc7ef583464c091f
KAMEGAWA Kazushi <kkamegawa@clueup.org>
2023-04-20 02:00:22 +0900

    Update test_wf.ps1

--- test_wf.ps1
+++ test_wf.ps1
@@ -1,6 +1,6 @@
 param(
     [String] [Parameter (Mandatory=$true)] $apitoken,
-    [String] [Parameter (Mandatory=$true)] $apiUrl,
+    [String] [Parameter (Mandatory=$true)] $apiUrl
 )
 
 if([string]::isnullorempty($apitoken) -eq $true){
