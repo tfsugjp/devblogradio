dc497f394951346d0bdc48393c2666567840600e
KAMEGAWA Kazushi <kkamegawa@clueup.org>
2023-04-20 10:25:04 +0900

    Delete test_wf.ps1

--- test_wf.ps1
+++
@@ -1,7 +0,0 @@
-if([string]::isnullorempty($env:OPEN_API_TOKEN) -eq $true){
-  write-host 'API Token is null'
-}
-
-if([string]::isnullorempty($env:OPEN_API_URL) -eq $true){
-  write-host 'API endpoint is null'
-}
