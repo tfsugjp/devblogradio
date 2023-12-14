ceb4d4cb124543c47154e13cdcadd805de173ddb
KAMEGAWA Kazushi <kkamegawa@clueup.org>
2023-12-14 21:45:49 +0900

    Refactor feed synchronization logic and fix date/time handling

--- feedsync.ps1
+++ feedsync.ps1
@@ -41,25 +41,25 @@ function Get-SummarywithOpenAI(
 
 $urls = Get-Content -Path .\feedsource.txt
 
+$number = (gh issue list -s open --json number | convertfrom-json).number
+$createdAt = [datetime](gh issue view $number --json createdAt | convertfrom-json).createdAt
+$comments = (gh issue view $number -c --json comments | convertfrom-json).comments
+# createdAt returns UTC, issue comments returns local time, why?
+
+if($comments.Count -gt 1){
+    $lastupdate = [datetime]($comments | Sort-Object $_.createdAt -Bottom 1 | Select-Object createdAt).createdAt
+    #gh issue returns local timezone
+    $currenttimediff = (Get-TimeZone).baseutcoffset.hours
+    $lastupdate = $lastupdate.addhours(-$currenttimediff.hours)
+}else{
+    $lastupdate = $createdAt
+}
+
 foreach($url in $urls) {
-    $number = (gh issue list -s open --json number | convertfrom-json).number
-    # createdAt returns UTC, issue comments returns local time, why?
-    $createdAt = [datetime](gh issue view $number --json createdAt | convertfrom-json).createdAt
-    
-    $comments = (gh issue view $number -c --json comments | convertfrom-json).comments
-    if($comments.Count -gt 1){
-        $lastupdate = [datetime]($comments | Sort-Object $_.createdAt -Bottom 1 | Select-Object createdAt).createdAt
-        #gh issue returns local timezone
-        $currenttimediff = (Get-TimeZone).baseutcoffset.hours
-        $lastupdate = $lastupdate.addhours(-$currenttimediff.hours)
-    }else{
-        $lastupdate = $createdAt
-    }
-    
     $feed =[xml](invoke-webrequest -Uri $url -UseBasicParsing)
     foreach ($item in $feed.rss.channel.item) {
         $pubDate = [datetime]$item.pubDate
-        if($item.creator -ne 'Raymond Chen' -and $pubDate -gt $lastupdate) {
+        if($pubDate -gt $lastupdate) {
             $title = $item.title
             $link = $item.link 
             $summary = Get-SummarywithOpenAI $link
